9651849d0ff931279c17aec94a500520485e9f29
janjao937 <janjao937@gmail.com>
2023-08-09 14:44:39 +0700

    edit printPrime

--- 01-Function-Declaration/05-printPrime/main.js
+++ 01-Function-Declaration/05-printPrime/main.js
@@ -5,22 +5,26 @@ function printPrime(range)//print
 {
     for(let i = 2 ; i<= range;i++)
     {
-        checkPrime(i);
+        //checkPrime(i);
+        if(checkPrime(i))
+        {
+            console.log(i);
+        }
     }
 }
 function checkPrime(num)//check
 {   
-    let isPrime = true;
+   // let isPrime = true;
     for(let j = 2;j<= Math.sqrt(num);j++)
         {
             if(num%j === 0)
             {
                 //Not Prime
-                isPrime = false;
-                break;
+                return false;
+                
             }
         }
-    if(isPrime) console.log(num);
+   return true;
 }
 
 // function printPrime(range)
